be93a35484915547037e6865f7baa96f9fb87459
RichardHayes <rhshadowgreen@gmail.com>
2024-02-18 20:47:51 -0800

    Progress. But there's something I don't understand about how zooming works in the real app

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+let tracker = {};
+
 (() => {
     
-    // The current font size
+    // Refs
+    const digitContainer = document.querySelector(".digit-container");
+    let allDigits = null; // lazy init
+    let curDigitSize = {width: null, height: null};
+
+    // Variables
     let fSize = 1.5;
+    let digitContainer_Offset_Target = {x: 0, y: 0};
+    let digitContainer_Offset = {x: 0, y: 0};
+    let activeAnim = {};
+
+    // Constants
+    const dir = {
+        'UP': {x: 0, y: -1},
+        'RIGHT': {x: 1, y: 0},
+        'DOWN': {x: 0, y: 1},
+        'LEFT': {x: -1, y: 0}
+    };
+
+    const uid = (() => {
+        let id = -1;
+        return () => {
+            id += 1;
+            return id;
+        };
+    })();
 
     function createDigit(n) {
         if (n === undefined) n = Math.floor(Math.random() * 10);
@@ -11,16 +37,25 @@
         return d;
     }
     
+    function render(timestamp) {
+        
+        for (let key in activeAnim) {
+            const {from, to, start, end} = activeAnim[key];
+
+        }
+
+    }
+
     function main() {
         
-        const dc = document.querySelector(".digit-container");
-        
-        Array(64 * 23).fill(null).forEach(e => {
-            dc.appendChild(
+        Array(64 * 40).fill(null).forEach(e => {
+            digitContainer.appendChild(
                 createDigit()
             );
         });
-
+        allDigits = document.querySelectorAll(".digit");
+        
+        window.requestAnimationFrame(render);
     }
 
     let once = false;
@@ -32,10 +67,29 @@
     };
 
     function fontSize(rem) {
-        document.querySelectorAll(".digit").forEach(e => {
+        allDigits.forEach(e => {
             e.style.fontSize = `${rem}rem`;
         });
 
+        // Maintain 64 in a row
+        curDigitSize = allDigits[0].getBoundingClientRect();
+        const { width } = curDigitSize;
+        const pad = 0;
+        digitContainer.style.width = `${64 * width + pad}px`;
+    }
+
+    function add(a, b) {
+        return {
+            x: a.x + b.x,
+            y: a.y + b.y
+        };
+    }
+
+    function mult(v, scalar) {
+        return {
+            x: v.x * scalar,
+            y: v.y * scalar
+        };
     }
 
     // debug font size
@@ -50,6 +104,25 @@
             fSize -= 0.25;
             fSize = Math.max(1.5, fSize);
             fontSize(fSize);
+
+        } else if ("wasd".indexOf(key) !== -1) {
+            const wasd_lookup = {
+                w: dir.UP,
+                d: dir.RIGHT,
+                s: dir.DOWN,
+                a: dir.LEFT
+            };
+            const {width, height} = curDigitSize;
+            const size_lookup = {
+                w: height,
+                d: width,
+                s: height,
+                a: width
+            };            
+            digitContainer_Offset_Target = add(
+                digitContainer_Offset_Target,
+                mult(wasd_lookup[key], size_lookup[key])
+            );
         }
     })
 
